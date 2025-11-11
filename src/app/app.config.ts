466b6887b1d29a0560a8609357bf0010493667fe
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2025-11-11 17:02:42 +0200

    added interceptors for header, error, loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,18 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { headerInterceptor } from './core/interceptors/header.interceptor';
+import { errorsInterceptor } from './core/interceptors/errors.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(),
+    provideHttpClient(withFetch(), withInterceptors([headerInterceptor, errorsInterceptor, loadingInterceptor])),
     provideAnimations(),
+    importProvidersFrom(NgxSpinnerModule),
   ],
 };
